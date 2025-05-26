dec5f034ef765d4f5a6f25dbc33adc7260857c88
leeseonggwon <gwondev0323@gmail.com>
2025-05-27 00:00:03 +0900

    1200

--- src/main/java/my/gwon/gwon/GwonApplication.java
+++ src/main/java/my/gwon/gwon/GwonApplication.java
@@ -2,10 +2,12 @@ package my.gwon.gwon;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class GwonApplication {
 
+	@GetMapping("/")
 	public static void main(String[] args) {
 		SpringApplication.run(GwonApplication.class, args);
 	}
