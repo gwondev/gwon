10d531d940ec22fe0bf597fe353cbc79c9229740
leeseonggwon <gwondev0323@gmail.com>
2025-07-20 14:30:51 +0900

    test

---
+++ src/test/java/my/gwon/GwonApplicationTests.java
@@ -0,0 +1,13 @@
+package my.gwon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GwonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
