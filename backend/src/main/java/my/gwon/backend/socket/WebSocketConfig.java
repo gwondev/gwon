acee69de3f4202175e702676f786fdae1c93397f
Your Name <your_email@example.com>
2025-07-30 01:59:49 +0900

    .withSockJS();

--- backend/src/main/java/my/gwon/backend/socket/WebSocketConfig.java
+++ backend/src/main/java/my/gwon/backend/socket/WebSocketConfig.java
@@ -10,7 +10,7 @@ public class WebSocketConfig implements WebSocketConfigurer {
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
         registry.addHandler(new DeviceWebSocketHandler(), "/socket")
-                .setAllowedOrigins("*");  // 혹은 "http://localhost:3000", "http://gwon.my"
+                .setAllowedOrigins("*").withSockJS();  // 혹은 "http://localhost:3000", "http://gwon.my"
     }
 }
 
