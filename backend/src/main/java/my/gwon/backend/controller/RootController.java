abf53358aeb504c3bd7e96997b120e3c3f4cdd56
leeseonggwon <gwondev0323@gmail.com>
2025-07-30 17:14:53 +0900

    plus pringln

--- backend/src/main/java/my/gwon/backend/controller/RootController.java
+++ backend/src/main/java/my/gwon/backend/controller/RootController.java
@@ -1,14 +1,17 @@
 package my.gwon.backend.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.stereotype.Controller;
 
 
 @Controller  
+@RequestMapping("/")
 public class RootController {
     
-    @GetMapping("/")
+    @GetMapping()
     public String root() {
+          System.out.println("Root controller 호출됨!"); // 이 로그가 나오는지 확인
         return "root";  // src/main/resources/templates/root.html 반환
     }
 }
