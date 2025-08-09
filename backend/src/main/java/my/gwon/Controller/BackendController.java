8cfda789cb66b13680388a636f960888eb9343e8
Your Name <your_email@example.com>
2025-08-09 14:01:07 +0900

    docker compose no chche 추가

--- backend/src/main/java/my/gwon/Controller/BackendController.java
+++ backend/src/main/java/my/gwon/Controller/BackendController.java
@@ -2,7 +2,7 @@ package my.gwon.Controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+
 
 @Controller
 
