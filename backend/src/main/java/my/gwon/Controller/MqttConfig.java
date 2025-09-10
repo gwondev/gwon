cab9f464e64a4b39a8334004a708b28b1dc4764d
gwon <gwondev0323@gmail.com>
2025-09-10 15:31:13 +0900

    plus mqtt config

---
+++ backend/src/main/java/my/gwon/Controller/MqttConfig.java
@@ -0,0 +1,47 @@
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.integration.annotation.ServiceActivator;
+import org.springframework.integration.channel.DirectChannel;
+import org.springframework.integration.core.MessageProducer;
+import org.springframework.integration.mqtt.core.DefaultMqttPahoClientFactory;
+import org.springframework.integration.mqtt.inbound.MqttPahoMessageDrivenChannelAdapter;
+import org.springframework.integration.mqtt.support.DefaultPahoMessageConverter;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.MessageHandler;
+
+@Configuration
+public class MqttConfig {
+
+    private final String brokerUrl = "tcp://gwon.my:1883";  // 브로커 주소
+    private final String clientId = "spring-client";        // 클라이언트 ID
+    private final String topic = "move/gps/Chosun/1";       // 구독할 토픽
+
+    @Bean
+    public DefaultMqttPahoClientFactory mqttClientFactory() {
+        return new DefaultMqttPahoClientFactory();
+    }
+
+    @Bean
+    public MessageChannel mqttInputChannel() {
+        return new DirectChannel();
+    }
+
+    @Bean
+    public MessageProducer inbound() {
+        MqttPahoMessageDrivenChannelAdapter adapter =
+                new MqttPahoMessageDrivenChannelAdapter(brokerUrl, clientId, topic);
+        adapter.setCompletionTimeout(5000);
+        adapter.setConverter(new DefaultPahoMessageConverter());
+        adapter.setQos(1);
+        adapter.setOutputChannel(mqttInputChannel());
+        return adapter;
+    }
+
+    @Bean
+    @ServiceActivator(inputChannel = "mqttInputChannel")
+    public MessageHandler handler() {
+        return message -> {
+            System.out.println("📡 MQTT 수신: " + message.getPayload());
+        };
+    }
+}
