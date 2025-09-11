101568e47141d2efa429ba615654573f35174d81
gwon <gwondev0323@gmail.com>
2025-09-12 01:31:36 +0900

    backend

--- backend/src/main/java/my/gwon/Controller/BackendController.java
+++ backend/src/main/java/my/gwon/Controller/BackendController.java
@@ -1,18 +1,18 @@
-package my.gwon.Controller;
+package my.gwon.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 
 @Controller
-
 public class BackendController {
     @GetMapping("/")
     public String backend(){
         return "backend";
     }
+
     @GetMapping("/move")
-    public String testMove() {
-        return "move-test"; // 단순히 문자열을 반환하여 정상작동 확인
+    public String move() {
+        return "backend"; // move.html 파일을 반환
     }
 }
