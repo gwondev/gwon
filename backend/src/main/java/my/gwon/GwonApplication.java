f4085df21e8403e248b6dffa34f252fd2f81fe39
gwon <gwondev0323@gmail.com>
2025-10-30 02:15:11 +0900

    back

--- backend/src/main/java/my/gwon/GwonApplication.java
+++ backend/src/main/java/my/gwon/GwonApplication.java
@@ -2,10 +2,8 @@ package my.gwon;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EnableJpaRepositories(basePackages = "my.gwon.repository")
 public class GwonApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(GwonApplication.class, args);
