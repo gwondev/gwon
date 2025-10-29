2be3c58b73ad5c0dcd3ff6d42e13c78f9317d64b
gwon <gwondev0323@gmail.com>
2025-10-30 02:01:17 +0900

    db heart

--- backend/src/main/java/my/gwon/GwonApplication.java
+++ backend/src/main/java/my/gwon/GwonApplication.java
@@ -2,8 +2,10 @@ package my.gwon;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "my.gwon.repository")
 public class GwonApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(GwonApplication.class, args);
--- backend/src/main/java/my/gwon/repository/HeartRepository.java
+++ backend/src/main/java/my/gwon/repository/HeartRepository.java
@@ -2,6 +2,8 @@ package my.gwon.repository;
 
 import my.gwon.entity.Heart;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface HeartRepository extends JpaRepository<Heart, String> {
 }
