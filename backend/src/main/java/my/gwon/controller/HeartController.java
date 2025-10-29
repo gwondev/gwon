d20a2efc160853032fb63dcd583aab0ba8175579
gwon <gwondev0323@gmail.com>
2025-10-30 01:39:09 +0900

    change db

--- backend/src/main/java/my/gwon/controller/HeartController.java
+++ backend/src/main/java/my/gwon/controller/HeartController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/backend/like")
+@CrossOrigin(origins = "*")
 @RequiredArgsConstructor
 public class HeartController {
 
